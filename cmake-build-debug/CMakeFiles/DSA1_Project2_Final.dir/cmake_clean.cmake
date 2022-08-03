086f3ddf2284637ca168806b835e25df707ded59
Chris Lee <Kanghyuk.lee@cooper.edu>
2022-08-03 05:46:20 -0400

    initial

---
+++ cmake-build-debug/CMakeFiles/DSA1_Project2_Final.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSA1_Project2_Final.dir/main.cpp.o"
+  "DSA1_Project2_Final"
+  "DSA1_Project2_Final.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DSA1_Project2_Final.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
